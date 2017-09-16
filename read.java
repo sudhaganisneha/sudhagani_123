92e55902f07dcd8620fc70bacd0eb967cb529992
sudhaganisneha <32012498+sudhaganisneha@users.noreply.github.com>
2017-09-16 16:33:34 +0530

    read and write
    read=read it will read and whereas write is given it will write what we want

---
+++ read.java
@@ -0,0 +1,15 @@
+class read
+{
+system.out.println("write");
+}
+class write
+{
+if(write==read)
+{
+system.out.problem("read");
+}
+public static void main()
+{
+write.obj=new(write);
+read.obj=new(read);
+}
