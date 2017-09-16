13456a868dbc5b10f300dc28120383478cf675b7
sudhaganisneha <32012498+sudhaganisneha@users.noreply.github.com>
2017-09-16 16:05:53 +0530

    Adding code to a given program
    This program  will help in understanding how method overloading and method overriding works

---
+++ man.java
@@ -0,0 +1,13 @@
+class man
+{
+system.out.println("a");
+}
+class women extends man
+{
+system.out.println("w");
+}
+public static void main(string args[])
+{
+man.obj=new(a);
+women.obj=new(b);
+} 
--- program1.java
+++ program1.java
@@ -1,2 +1,7 @@
-//method overloading has same class name but different parameters//
-//method overriding has same class name and same parameters//
+public class Program1 {
+system.out.println("car");
+}
+    public static void main(String[] args) {
+        program1.obj=new program1(car);
+    }
+    
